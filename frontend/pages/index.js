fb349a15c329a777df8039555da13be4bec3c42b
mjwSolver <mwiradinata@student.ciputra.ac.id>
2025-08-04 20:43:13 +0700

    removing Main to get rid of the weird white spacing at the top

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,7 +2,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="min-h-screen bg-gray-100">
-        <main className="container mx-auto px-4 py-8">{children}</main>
+        {/* <main className="container mx-auto px-4 py-8">{children}</main> */}
+        {children}
       </body>
     </html>
   );
